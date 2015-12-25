53b9b7d0219b51ab338dd41e5facce7c972c4669
sakanaeie <sakanaeie@gmail.com>
2015-12-25 16:30:45 +0900

    curlヘルパーを修正

--- app/Infrastructure/Curl.php
+++ app/Infrastructure/Curl.php
@@ -6,24 +6,36 @@
 
 namespace Githutil\Infrastructure;
 
+use \Githutil\Infrastructure\Logger;
+
 class Curl
 {
 	/**
 	 * 実行
 	 *
-	 * @param  string $url    URL
-	 * @param  array  $post   送信データ
-	 * @return string $result 結果
+	 * @param  string $url     URL
+	 * @param  array  $post    送信データ
+	 * @param  bool   $is_json JSONで送信するかどうか
+	 * @return string $result  結果
 	 */
-	public static function execute($url, $post)
+	public static function execute($url, $post, $is_json = true)
 	{
 		$ch = curl_init($url);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_POST,           true);
-		curl_setopt($ch, CURLOPT_POSTFIELDS,     json_encode($post));
-		curl_setopt($ch, CURLOPT_HTTPHEADER,     ['Content-Type: application/json']);
+
+		if ($is_json) {
+			curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post));
+			curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+		} else {
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+		}
+
 		$result = curl_exec($ch);
 		curl_close($ch);
+
+		Logger::info(print_r($result, true));
+
 		return $result;
 	}
 }
